e1ebcb11e97a120e3b86762fafe9c1d3c3b40511
suthari sai manikanta vivek <115859708+Mani9505764142@users.noreply.github.com>
2025-12-01 11:09:39 +0530

    Update client.js

--- inara-frontend/src/api/client.js
+++ inara-frontend/src/api/client.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change to live URL after deploy
+  baseURL: import.meta.env.VITE_API_URL + "/api",
+  // Example: "https://inara-sazc.onrender.com/api"
+  timeout: 15000,
+  withCredentials: false
 });
 
 export default api;
